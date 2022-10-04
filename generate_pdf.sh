f2f664cc88aa44997c80344cdb5f045af2168308
Julien Vitay <julien.vitay@informatik.tu-chemnitz.de>
2022-10-04 11:41:19 +0200

    Tue Oct  4 11:41:19 CEST 2022

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -3,4 +3,4 @@
 input_file=$1
 output_file="slides/pdf/$(basename -- $input_file .html).pdf"
 
-decktape automatic --size='2048x1536' $input_file $output_file
+decktape reveal --size='2048x1536' $input_file $output_file
